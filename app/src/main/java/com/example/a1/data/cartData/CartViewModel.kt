3463e8b89c78d48965e78dd45f0a54e3657ab3ea
KH01X <146006437+KH01X@users.noreply.github.com>
2024-05-19 19:54:38 +0800

    removed unused imports and functions from viewModel

--- app/src/main/java/com/example/a1/data/cartData/CartViewModel.kt
+++ app/src/main/java/com/example/a1/data/cartData/CartViewModel.kt
@@ -1,20 +1,16 @@
 package com.example.a1.data.cartData
 
-import android.graphics.BitmapFactory
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.nio.ByteBuffer
 
 
 data class Food(
@@ -141,19 +137,6 @@ class CartViewModel(private val cartRepository: CartRepository) : ViewModel() {
         removeFromDatabase(food)
     }
 
-    fun getTotalCartPrice(): Double {
-        return items.sumOf { it.price.toDoubleOrNull() ?: 0.0 }
-    }
-
-    fun getTotalCartItems(): Int {
-        var counter = 0
-        for (x in cart) {
-            counter++
-        }
-
-        return counter
-
-    }
 
     fun clearFoodCart() {
         items.clear()
@@ -184,23 +167,6 @@ class CartViewModel(private val cartRepository: CartRepository) : ViewModel() {
         isClearCartDialogShown = false
     }
 
-/*    fun intToByteArray(value: Int): ByteArray {
-        return ByteBuffer.allocate(4).putInt(value).array()
-    }
 
-    fun imageBitmapFromBytes(bytes: ByteArray): ImageBitmap {
-        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-        return bitmap.asImageBitmap()
-    }
-
-    fun byteArrayToInt(byteArray: ByteArray, littleEndian: Boolean = false): Int {
-        val buffer = ByteBuffer.wrap(byteArray)
-        if (littleEndian) {
-            buffer.order(java.nio.ByteOrder.LITTLE_ENDIAN)
-        } else {
-            buffer.order(java.nio.ByteOrder.BIG_ENDIAN)
-        }
-        return buffer.int
-    }*/
 
 }
